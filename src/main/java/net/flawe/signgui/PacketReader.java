ab38bec2e8a50d91acc5debcba8f5206571db826
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-01-21 01:20:59 +0300

    Some updates

--- src/main/java/net/flawe/signgui/PacketReader.java
+++ src/main/java/net/flawe/signgui/PacketReader.java
@@ -16,10 +16,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * The packet reader class
+ * @author flawe
+ */
 public class PacketReader {
     Channel channel;
     public static Map<UUID, Channel> channels = new HashMap<>();
 
+    /**
+     * Packet injection to player connection
+     * @param player - Player who's injected
+     */
     public void inject(Player player) {
 
         EntityPlayer entityPlayer = ((CraftPlayer)player).getHandle();
